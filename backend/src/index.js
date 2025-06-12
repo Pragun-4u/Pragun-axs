f8a59a06d068602989e6c8a8c6ac3587d090a120
Pragun <chhayakarande18@gmail.com>
2025-06-13 04:30:19 +0530

    port

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,8 +26,8 @@ const setupAndStartServer = async () => {
   app.use(cors());
   app.use("/api", router);
 
-  app.listen(8080, () => {
-    console.log("Server started on port 8080");
+  app.listen(process.env.PORT, () => {
+    console.log("Server started on port ", process.env.PORT);
   });
 };
 
